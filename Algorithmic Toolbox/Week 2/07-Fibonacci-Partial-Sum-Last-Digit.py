37adadfab181b840deff575f0142a104bc24d358
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2020-11-20 10:03:25 +0530

    Fibonacci Partial sum last digit done

---
+++ Algorithmic Toolbox/Week 2/07-Fibonacci-Partial-Sum-Last-Digit.py
@@ -0,0 +1,47 @@
+def fibonacci_partial_sum_naive(from_, to):
+    a=get_fibonacci_huge_naive(from_+1, 10) 
+    b=get_fibonacci_huge_naive(to+2, 10) 
+    c = b-a
+    if(c<0):
+    	return c + 10
+    else:
+    	return c	
+
+def pisanoperiod(a):
+    index=0
+    for i in range(1,len(a)):
+        if((a[i]==0) & (a[i+1]==1)):
+            break
+    index = i
+    return index
+
+def get_fibonacci(m): 
+    x = m*m+1
+    a = list()
+    a0=0
+    a1=1
+    a.append(a0)
+    a.append(a1)
+    for _ in range(x):
+        a0,a1 = a1,(a0+a1)%m
+        a.append(a1)
+    return(a)    
+
+def get_fibonacci_huge_naive(n, m):
+    if n <= 1:
+        return n
+    a = pisanoperiod(get_fibonacci(m))
+    previous = 0
+    current  = 1
+    g = n%a
+    if g <= 1:
+        return g
+
+    for i in range(g-1):
+        previous, current = current, previous + current
+        if i % 10 == 0:
+            previous, current =  previous , current
+    return (current) % m
+
+from_, to = map(int, input().split())
+print(fibonacci_partial_sum_naive(from_, to))
