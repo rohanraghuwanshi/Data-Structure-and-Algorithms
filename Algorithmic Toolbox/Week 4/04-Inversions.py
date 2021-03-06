fb22218ba3fafaf1d1df8defc3046fc146f0810c
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-03-06 23:05:31 +0530

    Inversions program done

---
+++ Algorithmic Toolbox/Week 4/04-Inversions.py
@@ -0,0 +1,36 @@
+def inversion_by_merge_sort(a):
+
+    if len(a)==1:
+        return a,0
+
+    mid = len(a)//2
+    left,left_count_inversion = inversion_by_merge_sort(a[:mid])
+    right,right_count_inversion = inversion_by_merge_sort(a[mid:])
+
+    i = 0
+    j = 0
+    c=[]
+    count_inversion = left_count_inversion+right_count_inversion
+
+    while(i<len(left) and j<len(right)):
+        if left[i]<right[j]:
+            c.append(left[i])
+            i+=1
+
+        elif left[i]>right[j]:
+            c.append(right[j])
+            j+=1
+            count_inversion += (len(left)-i)
+
+        else:
+            c.append(left[i])
+            i+=1
+
+    c += left[i:]
+    c += right[j:]
+
+    return c,count_inversion
+
+
+a = list(map(int, input().split()))
+print(inversion_by_merge_sort(a)[-1])
